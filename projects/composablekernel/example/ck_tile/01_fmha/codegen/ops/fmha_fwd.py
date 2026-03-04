cec8ad808ade66cb1ae2e91ba8d091c659aa0de0
Hosang Yoon <hosang.yoon@amd.com>
2026-03-04 11:46:36 +0000

    remove residue

--- projects/composablekernel/example/ck_tile/01_fmha/codegen/ops/fmha_fwd.py
+++ projects/composablekernel/example/ck_tile/01_fmha/codegen/ops/fmha_fwd.py
@@ -1230,8 +1230,6 @@ def get_factory(target: str):
     if target.startswith("gfx9"):
         return KernelComponentFactoryGfx9
 
-    if target.startswith("gfx115"):
-        return KernelComponentFactoryGfx115
     if target.startswith("gfx11"):
         return KernelComponentFactoryGfx11
     if target.startswith("gfx12"):
